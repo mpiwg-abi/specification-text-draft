7a0de8d42ae21c5c1e60301b0f3f3e56161f094a
Jeff Hammond <jeff.science@gmail.com>
2023-04-24 14:17:51 +0300

    i found this useful

--- c_int.c
+++ c_int.c
@@ -8,6 +8,7 @@
 
 #define PRINT(x)  printf("%12s = %20lld\n", #x, (long long int) x)
 #define UPRINT(x) printf("%12s = %20llu\n", #x, (unsigned long long int) x)
+#define SPRINT(x) printf("%30s = %2llu\n", #x, (unsigned long long int) x)
 
 int main(void)
 {
@@ -30,6 +31,19 @@ int main(void)
     PRINT(INTPTR_MAX);
     PRINT(INTPTR_MIN);
     UPRINT(UINTPTR_MAX);
+
+    SPRINT(sizeof(int));
+    SPRINT(sizeof(unsigned int));
+    SPRINT(sizeof(long int));
+    SPRINT(sizeof(unsigned long int));
+    SPRINT(sizeof(long long int));
+    SPRINT(sizeof(unsigned long long int));
+    SPRINT(sizeof(intptr_t));
+    SPRINT(sizeof(ptrdiff_t));
+    SPRINT(sizeof(void*));
+
+    typedef void* (*fptr)(void*);
+    SPRINT(sizeof(fptr));
 
     return 0;
 }
